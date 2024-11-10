acd08a8eff4a93550ad5b43fe6da1ee96f111b29
Gabo1234567890 <gabonenov@gmail.com>
2024-11-10 20:41:05 +0200

    init_db.py comments

--- app/init_db.py
+++ app/init_db.py
@@ -1,15 +1,16 @@
+# Този файл създава PostgreSQL БД, създава message таблицата и слага съобщение в таблицата
+
 import psycopg2
 
-# Connect to PostgreSQL
-conn = psycopg2.connect(
+conn = psycopg2.connect( # Установява връзка с PostgreSQL БД, използвайки същите параметри като app.py
     host="db",
     database="exampledb",
     user="exampleuser",
     password="examplepass"
 )
-cursor = conn.cursor()
+cursor = conn.cursor() # Създава курсорен обект, чрез който се изпълняват SQL заявки
 
-# Create the messages table
+# Това създава таблицата messages в БД, ако не съществува вече и я създава с 2 колони - id, която е с автоматично нарастващи цели числа с единица, които са ключ и message, която самото съобщение
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS messages (
         id SERIAL PRIMARY KEY,
@@ -17,10 +18,9 @@ cursor.execute('''
     )
 ''')
 
-# Insert a sample message
-cursor.execute('INSERT INTO messages (message) VALUES (%s) ON CONFLICT DO NOTHING', ('Hello, Docker with PostgreSQL!',))
+# Това записва в message колоната от messages таблицата съобщението в скобите
+cursor.execute('INSERT INTO messages (message) VALUES (%s) ON CONFLICT DO NOTHING', ('Example message',))
 
-# Commit the changes and close the connection
-conn.commit()
+conn.commit() # Запазва промените в БД
 cursor.close()
 conn.close()
